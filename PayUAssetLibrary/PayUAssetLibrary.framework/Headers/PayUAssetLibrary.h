1fe7aec81b687b9e6d1b9907935bbb2a1822dc9c
Umang Arya <umang.arya@payu.in>
2020-07-29 19:31:12 +0530

    Added asset library

---
+++ PayUAssetLibrary/PayUAssetLibrary.framework/Headers/PayUAssetLibrary.h
@@ -0,0 +1,19 @@
+//
+//  PayUAssetLibrary.h
+//  PayUAssetLibrary
+//
+//  Created by Umang Arya on 30/04/20.
+//  Copyright © 2020 PayU Payments Pvt Ltd. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for PayUAssetLibrary.
+FOUNDATION_EXPORT double PayUAssetLibraryVersionNumber;
+
+//! Project version string for PayUAssetLibrary.
+FOUNDATION_EXPORT const unsigned char PayUAssetLibraryVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <PayUAssetLibrary/PublicHeader.h>
+
+
